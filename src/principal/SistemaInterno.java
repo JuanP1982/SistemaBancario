ce4c8a9bb8436b4549888a5718ba6abca54c730b
Juan Pedro <juanpedrovsc@gmail.com>
2024-04-18 17:57:23 -0300

    Limpando SistemaInterno

--- src/principal/SistemaInterno.java
+++ src/principal/SistemaInterno.java
@@ -2,34 +2,16 @@ package principal;
 
 import java.io.IOException;
 
-import menus.MenuTransacoes;
-import menus.Menulogin;
 
+import menus.Menulogin;
 
 public class SistemaInterno {
 
 	public static void main(String[] args) throws IOException {
 
 		Menulogin menulogin = new Menulogin();
-		MenuTransacoes transacoes = new MenuTransacoes();
-		
-		menulogin.menuPrincipal();
-		
-	}
 
-//	private static boolean souCliente(String cpf, String senha) {
-//		for (Cliente cliente : clientes) {
-//			if (cliente.getCpf().equals(cpf) && cliente.getSenha().equals(senha)) {
-//				return true;
-//			} 
-//		} return false;
-//	}
-//	
-//	private static boolean souFuncionario (String cpf, String senha) {
-//		for (Funcionario funcionario : funcionarios) {
-//			if (funcionario.getCpf().equals(senha) && funcionario.getSenha().equals(senha)) {
-//				return funcionario;
-//			}
-//		} return false;
-//	}
+		menulogin.menuPrincipal();
+
+	}
 }
