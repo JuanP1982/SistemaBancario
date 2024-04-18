f38357a7be69cb1f777d0471b03b2c63151ea873
Juan Pedro <juanpedrovsc@gmail.com>
2024-04-18 20:15:02 -0300

    Atualização retirada do programa Final correto.

--- src/menus/MenuTransacoes.java
+++ src/menus/MenuTransacoes.java
@@ -31,50 +31,51 @@ public class MenuTransacoes {
 		System.out.println("Bem vindo ao menu de saque!");
 		System.out.println("Seu saldo é: " + cliente.getConta().getSaldo());
 		try {
-		
-		
-		System.out.println("Quanto deseja sacar?");
-		double valor = sc.nextDouble();
-		
-		if (valor > cliente.getConta().getSaldo() ) {
-            throw new SaqueNegativoException("Você não pode sacar! Seu saldo está zerado ou é negativo!");
-        } else if (valor < 0) {
-	    	System.err.println("Você não pode adcionar um valor negativo!");
-	    	login.menuPrincipal();
-	    }
-		cliente.getConta().saque(valor);
-		exSaque.setarValores(valor, cpf, LocalDateTime.now());
-		extrato.add(exSaque);
-		login.usuarios.put(cpf, cliente);
 
-		InOutUtils.escritorExtrato("../SistemaBancario/arquivos/extrato.txt", exSaque, extrato);
-		InOutUtils.escritorConta("../SistemaBancario/arquivos/contas.txt", (Conta) cliente.getConta(), login.contas);
+			System.out.println("Quanto deseja sacar?");
+			double valor = sc.nextDouble();
 
-		System.out.println("Seu saldo é: " + cliente.getConta().getSaldo());
-		System.out.println("1- Continuar | 2- Sair");
-		int escolha = sc.nextInt();
-		switch (escolha) {
-		case 1:
-			this.saque(cpf, extrato);
-		case 2:
-			if (login.usuarios.get(cpf).getTipo().equalsIgnoreCase("Cliente")) {
-				login.menuCliente(cpf);
-			} else if (login.usuarios.get(cpf).getTipo().equalsIgnoreCase("Gerente")) {
-				login.menuGerente(cpf);
-			} else if (login.usuarios.get(cpf).getTipo().equalsIgnoreCase("Diretor")) {
-				login.menuDiretor(cpf);
-			} else if (login.usuarios.get(cpf).getTipo().equalsIgnoreCase("Presidente")) {
-				login.menuPresidente(cpf);
+			if (valor > cliente.getConta().getSaldo()) {
+				throw new SaqueNegativoException("Você não pode sacar! Seu saldo está zerado ou é negativo!");
+			} else if (valor < 0) {
+				System.err.println("Você não pode adcionar um valor negativo!");
+				login.menuPrincipal();
 			}
-		} } catch (SaqueNegativoException e) {
-	        // Lidar com a exceção SaqueNegativoException
-	        System.err.println("Você não pode sacar um valor maior que seu saldo!");
-	        login.menuPrincipal();
-	        
-	    } catch (InputMismatchException e) {
-	    	System.err.println("Você não pode sacar este valor, cobramos uma taxa de 0.10 para saques!");
-	    	login.menuPrincipal();
-	    }
+			cliente.getConta().saque(valor);
+			exSaque.setarValores(valor, cpf, LocalDateTime.now());
+			extrato.add(exSaque);
+			login.usuarios.put(cpf, cliente);
+
+			InOutUtils.escritorExtrato("../SistemaBancario/arquivos/extrato.txt", exSaque, extrato);
+			InOutUtils.escritorConta("../SistemaBancario/arquivos/contas.txt", (Conta) cliente.getConta(),
+					login.contas);
+
+			System.out.println("Seu saldo é: " + cliente.getConta().getSaldo());
+			System.out.println("1- Continuar | 2- Sair");
+			int escolha = sc.nextInt();
+			switch (escolha) {
+			case 1:
+				this.saque(cpf, extrato);
+			case 2:
+				if (login.usuarios.get(cpf).getTipo().equalsIgnoreCase("Cliente")) {
+					login.menuCliente(cpf);
+				} else if (login.usuarios.get(cpf).getTipo().equalsIgnoreCase("Gerente")) {
+					login.menuGerente(cpf);
+				} else if (login.usuarios.get(cpf).getTipo().equalsIgnoreCase("Diretor")) {
+					login.menuDiretor(cpf);
+				} else if (login.usuarios.get(cpf).getTipo().equalsIgnoreCase("Presidente")) {
+					login.menuPresidente(cpf);
+				}
+			}
+		} catch (SaqueNegativoException e) {
+			// Lidar com a exceção SaqueNegativoException
+			System.err.println("Você não pode sacar um valor maior que seu saldo!");
+			this.saque(cpf, extrato);
+
+		} catch (InputMismatchException e) {
+			System.err.println("Você não pode sacar este valor, cobramos uma taxa de 0.10 para saques!");
+			this.saque(cpf, extrato);
+		}
 
 	}
 
@@ -85,49 +86,51 @@ public class MenuTransacoes {
 
 		System.out.println("Bem vindo ao menu de Depositos!");
 		System.out.println("Seu saldo é: " + cliente.getConta().getSaldo());
-		
+
 		try {
-		
-		
-		System.out.println("Quanto deseja depositar?");
-		double valor = sc.nextDouble();
-		
-		if (valor <= 0) {
-            throw new IllegalArgumentException("Você não pode depositar um valor negativo ou zero.");
-        } 
-		cliente.getConta().deposito(valor);
 
-		exDeposito.setarValores(valor, cpf, LocalDateTime.now());
-		extrato.add(exDeposito);
-		System.out.println("Seu saldo é: " + cliente.getConta().getSaldo());
+			System.out.println("Quanto deseja depositar?");
+			double valor = sc.nextDouble();
 
-		InOutUtils.escritorExtrato("../SistemaBancario/arquivos/extrato.txt", exDeposito, extrato);
-		InOutUtils.escritorConta("../SistemaBancario/arquivos/contas.txt", (Conta) cliente.getConta(), login.contas);
-		System.out.println("1- Continuar | 2- Sair");
-		int escolha = sc.nextInt();
-		switch (escolha) {
-		case 1:
-			this.deposito(cpf, extrato);
-		case 2:
-			if (login.usuarios.get(cpf).getTipo().equalsIgnoreCase("Cliente")) {
-				login.menuCliente(cpf);
-			} else if (login.usuarios.get(cpf).getTipo().equalsIgnoreCase("Gerente")) {
-				login.menuGerente(cpf);
-			} else if (login.usuarios.get(cpf).getTipo().equalsIgnoreCase("Diretor")) {
-				login.menuDiretor(cpf);
-			} else if (login.usuarios.get(cpf).getTipo().equalsIgnoreCase("Presidente")) {
-				login.menuPresidente(cpf);
+			if (valor <= 0) {
+				throw new IllegalArgumentException("Você não pode depositar um valor negativo ou zero.");
 			}
-		} } catch (IllegalArgumentException e) {
-	        // Lidar com a exceção de valor negativo ou zero
-	        System.err.println(e.getMessage());
-	        // Chamar o método deposito() novamente se necessário
-	        this.deposito(cpf, extrato);
-	    }
+			cliente.getConta().deposito(valor);
+
+			exDeposito.setarValores(valor, cpf, LocalDateTime.now());
+			extrato.add(exDeposito);
+			System.out.println("Seu saldo é: " + cliente.getConta().getSaldo());
+
+			InOutUtils.escritorExtrato("../SistemaBancario/arquivos/extrato.txt", exDeposito, extrato);
+			InOutUtils.escritorConta("../SistemaBancario/arquivos/contas.txt", (Conta) cliente.getConta(),
+					login.contas);
+			System.out.println("1- Continuar | 2- Sair");
+			int escolha = sc.nextInt();
+			switch (escolha) {
+			case 1:
+				this.deposito(cpf, extrato);
+			case 2:
+				if (login.usuarios.get(cpf).getTipo().equalsIgnoreCase("Cliente")) {
+					login.menuCliente(cpf);
+				} else if (login.usuarios.get(cpf).getTipo().equalsIgnoreCase("Gerente")) {
+					login.menuGerente(cpf);
+				} else if (login.usuarios.get(cpf).getTipo().equalsIgnoreCase("Diretor")) {
+					login.menuDiretor(cpf);
+				} else if (login.usuarios.get(cpf).getTipo().equalsIgnoreCase("Presidente")) {
+					login.menuPresidente(cpf);
+				}
+			}
+		} catch (IllegalArgumentException e) {
+			// Lidar com a exceção de valor negativo ou zero
+			System.err.println(e.getMessage());
+			// Chamar o método deposito() novamente se necessário
+			this.deposito(cpf, extrato);
+		}
 
 	}
 
-	public void transferencia(String cpf, Map<String, Usuarios> usuarios, List<Extrato> extrato) throws IOException, IllegalArgumentException {
+	public void transferencia(String cpf, Map<String, Usuarios> usuarios, List<Extrato> extrato)
+			throws IOException, IllegalArgumentException {
 		ExtratoTransferencias exTransf = new ExtratoTransferencias(null, null, 0, null, null, null);
 		Usuarios cliente = usuarios.get(cpf);
 		usuarios.get(cpf);
@@ -138,55 +141,58 @@ public class MenuTransacoes {
 		System.out.println("Bem vindo ao menu de Transação||Transferencia!");
 		System.out.println("Seu saldo é: " + String.format("%.2f", cliente.getConta().getSaldo()));
 		try {
-		System.out.println("Escreva o CPF de quem deseja transferir!");
-		cpf2 = sc.next();
-		if (usuarios.containsKey(cpf2) == false) {
-			
-			throw new IllegalArgumentException("CPF não encontrado!");
-		};
-		System.out.println("Quanto deseja transferir?");
-		valor = sc.nextDouble();
-		
-		if (valor > cliente.getConta().getSaldo()) {
-	        System.err.println("Você não pode transferir um valor maior que seu saldo disponível.");
-	        login.menuPrincipal();; // Retorna imediatamente se a transferência não for possível
-	    } 
-		cliente.getConta().transacaoEnviar(valor);
-		exTransf.setarValores(valor, cpf, LocalDateTime.now(), cpf2);
-		usuarios.get(cpf2).getConta().transacaoReceber(valor);
+			System.out.println("Escreva o CPF de quem deseja transferir!");
+			cpf2 = sc.next();
+			if (usuarios.containsKey(cpf2) == false) {
 
-		InOutUtils.escritorConta("../SistemaBancario/arquivos/contas.txt", (Conta) cliente.getConta(), login.contas);
-		System.out.println(String.format("%.2f", cliente.getConta().getSaldo()));
-		System.out.println("Você transferiu o valor de "+ valor + " para  " + usuarios.get(cpf2).getNome());
-		InOutUtils.escritorExtrato("../SistemaBancario/arquivos/extrato.txt", exTransf, extrato);
-		System.out.println("1- Continuar | 2- Sair");
-		int escolha2 = sc.nextInt();
-		switch (escolha2) {
-		case 1:
-			this.transferencia(cpf, usuarios, extrato);
-		case 2:
-			if (usuarios.get(cpf).getTipo().equalsIgnoreCase("Cliente")) {
-				login.menuCliente(cpf);
-			} else if (usuarios.get(cpf).getTipo().equalsIgnoreCase("Gerente")) {
-				login.menuGerente(cpf);
-			} else if (usuarios.get(cpf).getTipo().equalsIgnoreCase("Diretor")) {
-				login.menuDiretor(cpf);
-			} else if (usuarios.get(cpf).getTipo().equalsIgnoreCase("Presidente")) {
-				login.menuPresidente(cpf);
+				throw new IllegalArgumentException("CPF não encontrado!");
 			}
-		} } catch (IllegalArgumentException e) {
-	        // Lidar com a exceção de valor negativo ou zero
-	        System.err.println(e.getMessage());
-	        
-	        
-	    }
+			;
+			System.out.println("Quanto deseja transferir?");
+			valor = sc.nextDouble();
+
+			if (valor > cliente.getConta().getSaldo()) {
+				System.err.println("Você não pode transferir um valor maior que seu saldo disponível.");
+				login.menuPrincipal();
+				; // Retorna imediatamente se a transferência não for possível
+			}
+			cliente.getConta().transacaoEnviar(valor);
+			exTransf.setarValores(valor, cpf, LocalDateTime.now(), cpf2);
+			usuarios.get(cpf2).getConta().transacaoReceber(valor);
+
+			InOutUtils.escritorConta("../SistemaBancario/arquivos/contas.txt", (Conta) cliente.getConta(),
+					login.contas);
+			System.out.println(String.format("%.2f", cliente.getConta().getSaldo()));
+			System.out.println("Você transferiu o valor de " + valor + " para  " + usuarios.get(cpf2).getNome());
+			InOutUtils.escritorExtrato("../SistemaBancario/arquivos/extrato.txt", exTransf, extrato);
+			System.out.println("1- Continuar | 2- Sair");
+			int escolha2 = sc.nextInt();
+			switch (escolha2) {
+			case 1:
+				this.transferencia(cpf, usuarios, extrato);
+			case 2:
+				if (usuarios.get(cpf).getTipo().equalsIgnoreCase("Cliente")) {
+					login.menuCliente(cpf);
+				} else if (usuarios.get(cpf).getTipo().equalsIgnoreCase("Gerente")) {
+					login.menuGerente(cpf);
+				} else if (usuarios.get(cpf).getTipo().equalsIgnoreCase("Diretor")) {
+					login.menuDiretor(cpf);
+				} else if (usuarios.get(cpf).getTipo().equalsIgnoreCase("Presidente")) {
+					login.menuPresidente(cpf);
+				}
+			}
+		} catch (IllegalArgumentException e) {
+			// Lidar com a exceção de valor negativo ou zero
+			System.err.println(e.getMessage());
+			this.transferencia(cpf, usuarios, extrato);
+
+		}
 	}
 
 	public void extrato(String cpf, Map<String, Usuarios> usuarios, List<Extrato> extratos) throws IOException {
 		Usuarios cliente = usuarios.get(cpf);
 		usuarios.get(cpf);
 		int escolha = 0;
-		System.out.println(extratos);
 		System.out.println("        Bem-vindo ao menu de extrato");
 		System.out.println("        Qual extrato você que ver?\n        " + " 1- Saques\n        "
 				+ " 2- Depositos\n        " + " 3- Transferências\n        " + " 4- Sair");
@@ -202,7 +208,7 @@ public class MenuTransacoes {
 					System.out.println("Valor: " + extrato.getValor());
 
 				}
-			} 
+			}
 			break;
 		case 2:
 			System.out.println("Extratos de Depositos:");
@@ -213,29 +219,27 @@ public class MenuTransacoes {
 					System.out.println("Valor: " + extrato.getValor());
 
 				}
-			} 
+			}
 			break;
 		case 3:
-			
+
 			for (Extrato extrato : extratos) {
 				if (extrato.getCpfTitular().equals(cpf)
-					&& extrato.getTipo().equalsIgnoreCase(TransacoesEnum.Transferencias.name())) {
+						&& extrato.getTipo().equalsIgnoreCase(TransacoesEnum.Transferencias.name())) {
 					System.out.println("Tipo: " + extrato.getTipo());
 					System.out.println("Valor: " + extrato.getValor());
-				} 
-					
-				} 
-			
-			break;
-			} 
-			
-			
+				}
 
+			}
+
+			break;
+		}
 		System.out.println("1- Continuar | 2- Sair");
 		int escolha2 = sc.nextInt();
 		switch (escolha2) {
 		case 1:
 			this.extrato(cpf, usuarios, extratos);
+			break;
 		case 2:
 			if (usuarios.get(cpf).getTipo().equalsIgnoreCase("Cliente")) {
 				login.menuCliente(cpf);
@@ -246,6 +250,7 @@ public class MenuTransacoes {
 			} else if (usuarios.get(cpf).getTipo().equalsIgnoreCase("Presidente")) {
 				login.menuPresidente(cpf);
 			}
+			break;
 		}
 	}
 
@@ -265,17 +270,16 @@ public class MenuTransacoes {
 		int escolha = sc.nextInt();
 		if (escolha == 1) {
 			if (cliente.getConta().getSaldo() >= valorcon) {
-                cliente.getConta().setSaldo(novoSaldo);
-                System.out.println("Seguro contratado com sucesso!");
-            } else {
-                System.out.println("Saldo insuficiente para adesão de plano.");
-            }
-        } else if (escolha == 2) {
-            System.out.println("Operação cancelada.");
-        } else {
-            System.out.println("Opção inválida.");
-        }
-
+				cliente.getConta().setSaldo(novoSaldo);
+				System.out.println("Seguro contratado com sucesso!");
+			} else {
+				System.out.println("Saldo insuficiente para adesão de plano.");
+			}
+		} else if (escolha == 2) {
+			System.out.println("Operação cancelada.");
+		} else {
+			System.out.println("Opção inválida.");
+		}
 
 		InOutUtils.escritorConta("../SistemaBancario/arquivos/contas.txt", (Conta) cliente.getConta(), login.contas);
 		System.out.println(usuarios.get(cpf).getNome() + " " + usuarios.get(cpf).getConta().getSaldo());
